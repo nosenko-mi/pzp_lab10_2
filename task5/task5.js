af33fc79f1e8392c32194f9f117ce241bd83e5ec
nickolay <nosenkomi02@gmail.com>
2023-04-11 13:07:02 +0300

    refactor

--- task5/task5.js
+++ task5/task5.js
@@ -4,7 +4,7 @@ function getGreeting(hour) {
     let greeting = ''
     if (hour < 12) {
         greeting = 'Good morning!'
-    } else if (hour > 18) {
+    } else if (hour < 18) {
         greeting = 'Good afternoon'
     } else {
         greeting = 'Good evening'
--- task7/task7.js
+++ task7/task7.js
@@ -8,30 +8,35 @@ class Calculator {
     read() {
         this._a = parseFloat(prompt('Enter first number'))
         this._b = parseFloat(prompt('Enter second number'))
+        console.log(`read: a = ${this._a}; b = ${this._b}`)
         return this
     }
 
     sum() {
         this.result = this._a + this._b
         alert(this.result)
+        console.log(`sum: ${this.result}`)
         return this
     }
 
     sub() {
         this.result = this._a - this._b
         alert(this.result)
+        console.log(`sub: ${this.result}`)
         return this
     }
 
     mul() {
         this.result = this._a * this._b
         alert(this.result)
+        console.log(`mul: ${this.result}`)
         return this
     }
 
     div() {
         this.result = this._a / this._b
         alert(this.result)
+        console.log(`div: ${this.result}`)
         return this
     }
 }
